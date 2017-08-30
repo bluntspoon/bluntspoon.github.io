d176d473bb205f95a40f188c40b2c5b660919121
Andrew Bevan <bluntspoon@gmail.com>
2017-08-30 20:21:21 +0200

    moved the install

--- js/init.js
+++ js/init.js
@@ -5,19 +5,6 @@
 */
 
 (function() {
-
-
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/sw.js', {scope: '/'})
-		.then(function(reg) {
-		  // registration worked
-		  console.log('Registration succeeded. Scope is ' + reg.scope);
-		}).catch(function(error) {
-		  // registration failed
-		  console.log('Registration failed with ' + error);
-		});
-	  }
-
 	skel.init({
 		reset: 'full',
 		breakpoints: {
