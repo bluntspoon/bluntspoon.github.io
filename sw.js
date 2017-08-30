f5c4e9f03a92179819f85efc239361caf28a9e71
Andrew Bevan <bluntspoon@gmail.com>
2017-08-30 20:35:03 +0200

    update sw

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
+      caches.open('v2').then(function(cache) {
         return cache.addAll([
           '/',
           '/index.html',
