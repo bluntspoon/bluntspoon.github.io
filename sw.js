74d06c573be7754d8ab25bb68495909d08efb236
Andrew Bevan <bluntspoon@gmail.com>
2017-08-30 20:53:34 +0200

    major update to sw

--- sw.js
+++ sw.js
@@ -1,14 +1,51 @@
-self.addEventListener('install', function(event) {
+var cacheWhitelist = ['cache-v1', 'cache-v2'];
+
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/main-layout.css',
+  '/css/style.css',
+  '/css/images/bg.jpg',
+  '/assets/images/andrew-bevan.png',
+  '/js/skel.min.js'
+];
+
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+
+self.addEventListener('activate', function(event) {
     event.waitUntil(
-      caches.open('v2').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/css/style.css',
-          '/css/images/bg.jpg'        
-        ]);
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
       })
     );
   });
-
-
