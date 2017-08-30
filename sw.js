e75eb169e91ab329984f7ed1017655d51c5fd55d
Andrew Bevan <bluntspoon@gmail.com>
2017-08-30 20:57:31 +0200

    add cache

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-var cacheWhitelist = ['cache-v1', 'cache-v2'];
-
+var CACHE_NAME = 'cache-v1';
 var urlsToCache = [
   '/',
   '/index.html',
@@ -21,31 +20,31 @@ self.addEventListener('install', function (event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    .then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
       }
-    )
+      return fetch(event.request);
+    })
   );
 });
 
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = ['cache-v1', 'cache-v2'];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
