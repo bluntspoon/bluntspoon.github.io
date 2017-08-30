85d27b2abb2911b3308dbaf66b18accfe292e7eb
Andrew Bevan <bluntspoon@gmail.com>
2017-08-30 20:18:53 +0200

    moved sw

--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@
 
 
 	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/js/sw.js', {scope: '/'})
+		navigator.serviceWorker.register('/sw.js', {scope: '/'})
 		.then(function(reg) {
 		  // registration worked
 		  console.log('Registration succeeded. Scope is ' + reg.scope);
